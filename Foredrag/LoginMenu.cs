69c17641c594ff25cfe28bbc34609c3d67740843
Ayanokino <xekrs2000@gmail.com>
2024-01-08 14:55:54 +0100

    first commit

---
+++ Foredrag/LoginMenu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Foredrag_Login
+{
+    public class LoginMenu
+    {
+        public string FilePath { get; set; }
+        public string ID { get; set; }
+        public string Navn { get; set; }
+        public string Level { get; set; }
+        public string Alder { get; set; }
+
+        public static void LoginInfo(string SchoolID, string Name)
+        {
+            LoginMenu Path = new LoginMenu();
+            Path.FilePath = @"C:\Users\lucpin\Desktop\Foredrag\LoginKonto.txt";
+
+            string[] lines = File.ReadAllLines(Path.FilePath);
+
+            foreach (string line in lines)
+            {
+
+                string[] parts = line.Split(',');
+                if (parts.Length == 4)
+                {
+                  string CorrectID = parts[0];
+                  Path.Navn = parts[1];
+                  Path.Alder =parts[2];
+                  Path.Level = parts[3];
+                }
+                if ()
+                {
+
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Foredrag/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Foredrag_Opgave
+{
+    public class Lokale
+    {
+        public string LokaleName = "TEC_GF2";
+    }
+        public class Foredrag : Lokale
+        {
+            public int Antal_Elever = 10;
+            public string Information { get; set; }
+
+            public string MatLære = "Mikkel";
+
+            public string FyLære = "Pernille";
+
+        }
+        public class Person
+        {
+            public string[] Name ={"Lucas","Clement","Johan","Sofia","Salih","Andrea","Victoria","Martin","Caio","Hans","Mikkel","Pernille"};
+        }
+           public class Lære : Person
+            {
+            public string Fag { get; set; }
+            public string ForedragList { get; set; }
+            }
+            public class Elev : Person
+            {
+                public string ForedragListElev { get; set; }
+            }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool MenuLoop = false;
+            do
+            {
+                Foredrag_Login.LoginMenu login = new Foredrag_Login.LoginMenu();
+                Console.WriteLine("ID: ");
+                login. = Console.ReadLine();
+                Console.WriteLine("Navn: ");
+
+            }
+            while (MenuLoop == false);
+           Foredrag foredrag = new Foredrag();
+           Console.WriteLine("Skrive dit foredrag");
+           foredrag.Information = Console.ReadLine();
+           Lære lære = new Lære();
+           Console.WriteLine("Skrive hvem der er læren");
+           Console.WriteLine("Du har Mikkel og Pernille");
+            
+           
+           File.WriteAllText("ForedragInformation.txt", foredrag.Information);
+           Console.WriteLine();
+           
+           /*Lære list = new Lære();
+           Elev listElev = new Elev();
+           list.ForedragList = foredrag.Information;
+           listElev.ForedragListElev = foredrag.Information;*/
+
+            Console.ReadKey();
+        }
+    }
+}
