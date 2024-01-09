479198e23d2e5e02ff2afe144947d66c22d8f9f0
Ayanokino <xekrs2000@gmail.com>
2024-01-09 14:50:08 +0100

    second

---
+++ Foredrag/AdminForedrag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Foredrag_Login
+{
+    public class LoginMenu
+    {
+        public string FilePath { get; set; }
+        public string ID { get; set; }
+        public string Navn { get; set; }
+        public string Level { get; set; }
+        public string Alder { get; set; }
+        public string InputID { get; set; }
+        public string NavnInput { get; set; }
+
+        public static void LoginInfo()
+        {
+
+        }
+    }
+}
--- Foredrag/Program.cs
+++ Foredrag/Program.cs
@@ -7,7 +7,7 @@ using System.IO;
 
 namespace Foredrag_Opgave
 {
-    public class Lokale
+    /*public class Lokale
     {
         public string LokaleName = "TEC_GF2";
     }
@@ -32,32 +32,106 @@ namespace Foredrag_Opgave
             }
             public class Elev : Person
             {
-                public string ForedragListElev { get; set; }
+                public string ForedragListElev { get; set; }*/
             }
-    
-    class Program
+
+class Program
+{
+   
+    public string InputID { get; set; }
+    public string NavnInput { get; set; }
+    public static int chooseAdmin { get; set; }
+    public static void Main(string[] args)
+
     {
-        static void Main(string[] args)
+
+        bool LoginLoop = false;
+        bool MenuLoop = false;
+        Foredrag_Login.LoginMenu login = new Foredrag_Login.LoginMenu();
+
+        login.FilePath = @"C:\Users\lucpin\Desktop\Foredrag\LoginKonto.txt";
+
+        string[] lines = File.ReadAllLines(login.FilePath);
+
+        foreach (string line in lines)
         {
-            bool MenuLoop = false;
-            do
+            string[] parts = line.Split(',');
+            if (parts.Length == 4)
             {
-                Foredrag_Login.LoginMenu login = new Foredrag_Login.LoginMenu();
-                Console.WriteLine("ID: ");
-                login. = Console.ReadLine();
-                Console.WriteLine("Navn: ");
+                login.ID = parts[0];
+                login.Navn = parts[1];
+                login.Alder = parts[2];
+                login.Level = parts[3];
+            }
+        }
+        do
+        {
+
+            Console.WriteLine("ID: ");
+            login.InputID = Console.ReadLine();
+            Console.WriteLine("Navn: ");
+            login.NavnInput = Console.ReadLine();
+
+            if (login.InputID == login.ID && login.NavnInput == login.Navn)
+            {
+                LoginLoop = true;
+            }
+            else
+            {
+                Console.WriteLine("Something is wrong");
+                Console.ReadKey();
+                Console.Clear();
+                
 
             }
-            while (MenuLoop == false);
-           Foredrag foredrag = new Foredrag();
-           Console.WriteLine("Skrive dit foredrag");
-           foredrag.Information = Console.ReadLine();
-           Lære lære = new Lære();
+        }
+        while (LoginLoop == false);
+        Console.Clear();
+        do
+        {
+            if (login.Level == "2")
+            {
+                Console.WriteLine($"ID:{login.ID}   Navn:{login.Navn}   Alder:{login.Alder}   Level:{login.Level}");
+                Console.WriteLine("\n1.Lave et foredrag");
+                Console.WriteLine("\n9.Exit");
+
+                try
+                {
+                    chooseAdmin = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Skrive igen");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                switch (chooseAdmin)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine($"ID:{login.ID}   Navn:{login.Navn}   Alder:{login.Alder}   Level:{login.Level}");
+
+                        break;
+                    case 9:
+                        MenuLoop = true;
+                        break;
+                }
+
+            }
+        }
+        while (MenuLoop == false);
+  
+
+        /*Console.WriteLine("Skrive dit foredrag");
+           string foredrag = Console.ReadLine();
+          
            Console.WriteLine("Skrive hvem der er læren");
            Console.WriteLine("Du har Mikkel og Pernille");
             
            
-           File.WriteAllText("ForedragInformation.txt", foredrag.Information);
+           File.WriteAllText("ForedragInformation.txt", foredrag);
            Console.WriteLine();
            
            /*Lære list = new Lære();
@@ -65,7 +139,8 @@ namespace Foredrag_Opgave
            list.ForedragList = foredrag.Information;
            listElev.ForedragListElev = foredrag.Information;*/
 
-            Console.ReadKey();
-        }
+        Console.ReadKey();
+        
     }
 }
+
